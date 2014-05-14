afee5296f2659fcc759e1e1e4e845267a8d4d79d
m0r17z <moritz_august@web.de>
2014-05-14 12:19:45 +0200

    bug fixes

--- mlp_eval.py
+++ mlp_eval.py
@@ -12,6 +12,7 @@ import theano.tensor as T
 import h5py as h5
 
 from alchemie import contrib
+from breze.learn.data import one_hot
 
 
 def evaluate_mlp(args):
@@ -19,13 +20,15 @@ def evaluate_mlp(args):
     data_dir = os.path.abspath(args['<data>'])
     os.chdir(dir)
     cps = contrib.find_checkpoints('.')
+
     if cps:
         with gzip.open(cps[-1], 'rb') as fp:
                 trainer = cPickle.load(fp)
                 trainer.model.parameters.data[...] = trainer.best_pars
-                data = h5.File(os.path.join(data_dir,'usarray_data_scaled_train_val_bin.hdf5','r'))
+                data = h5.File(os.path.join(data_dir,'usarray_data_scaled_train_val_bin.hdf5'),'r')
                 VX = data['validation_set/val_set'][...][:140000]
                 VZ = data['validation_labels/bin_val_labels'][...][:140000]
+                VZ = one_hot(VZ,2)
 
                 n_wrong = 1 - T.eq(T.argmax(trainer.model.exprs['output'], axis=1),
                                    T.argmax(trainer.model.exprs['target'], axis=1)).mean()
