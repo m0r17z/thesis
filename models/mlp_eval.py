2192c008d508618de042b82f9a8e198803ab224b
m0r17z <moritz_august@web.de>
2014-05-28 18:08:41 +0200

    some little changes in the eval script

--- models/mlp_eval.py
+++ models/mlp_eval.py
@@ -28,6 +28,7 @@ def evaluate_mlp(args):
                 trainer.model.parameters.data[...] = trainer.best_pars
                 data = h5.File(os.path.join(data_dir,'usarray_data_scaled_train_val_test_real.hdf5'),'r')
                 TX = data['test_set/test_set']
+                TA = data['test_annotations/test_annotations']
                 TZ = data['test_labels/real_test_labels']
                 TZ = one_hot(TZ,13)
 
@@ -38,14 +39,16 @@ def evaluate_mlp(args):
                 result = f_n_wrong(TX,TZ)
                 result_s = 'model achieved %f%% classification error on the test set' %(result)
 
-                indices = np.random.rand(50) * 270000
+                indices = np.random.rand(50) * 90000
                 for i in np.arange(50):
                     index = indices[i]
-                    prediction = trainer.model.predict(TX[index])
+                    prediction = trainer.model.predict(np.reshape(TX[index],(1,len(TX[index]))))
                     if np.argmax(prediction) == np.argmax(TZ[index]):
-                        result_s += '\nprediction for sample at position %d is %s, correct' %(index, str(prediction))
+                        result_s += '\nprediction for sample with annotation %s at position %d is %d with %.3f certainty, correct' \
+                                    %(str(TA[index]), index, np.argmax(prediction[0]),prediction[0][np.argmax(prediction[0])])
                     else:
-                        result_s += '\nprediction for sample at position %d is %s, wrong' %(index, str(prediction))
+                        result_s += '\nprediction for sample with annotation %s at position %d is %d with %.3f certainty, correct label %d had %.3f certainty' \
+                                    %(str(TA[index]), index, np.argmax(prediction[0]),prediction[0][np.argmax(prediction[0])], np.argmax(TZ[index]),prediction[0][np.argmax(TZ[index])])
 
                 print result_s
                 with open(os.path.join(dir,'eval_mlp_real_result.txt'),'w') as f:
