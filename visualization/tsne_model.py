1ef10d7da66073b810095c09b00090e6b7d3707e
m0r17z <moritz_august@web.de>
2014-09-09 14:21:06 +0200

    ..

--- visualization/tsne_model.py
+++ visualization/tsne_model.py
@@ -15,7 +15,7 @@ from breze.learn.tsne import Tsne
 from alchemie import contrib
 from breze.learn.data import one_hot
 
-def visualize_tsne():
+def visualize_tsne(args):
     model_dir = os.path.abspath(args['<model>'])
     data_dir = os.path.abspath(args['<data>'])
     os.chdir(model_dir)
