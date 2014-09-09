aa315c9558dd2a67cbf1c414ddd8327576d35b1f
m0r17z <moritz_august@web.de>
2014-09-09 14:13:37 +0200

    ..

---
+++ visualization/tsne_data.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""Usage: tsne_model.py <data>
+
+"""
+
+import cPickle
+import docopt
+import os
+import matplotlib.pyplot as plt
+import gzip
+import h5py as h5
+
+from breze.learn.tsne import Tsne
+from alchemie import contrib
+from breze.learn.data import one_hot
+
+def visualize_tsne():
+    data_dir = os.path.abspath(args['<data>'])
+
+    data = h5.File(data_dir,'r')
+    TX = data['test_set/test_set'][...]
+    TZ = data['test_labels/real_test_labels']
+    TZ = one_hot(TZ,13)
+    n_input = TX.shape[1]
+    print 'data loaded.'
+    tsne = Tsne(n_input, 2)
+    print 'TSNE initialized.'
+    TX_r = tsne.fit_transform(TX)
+    print 'data TSNEd.'
+
+    fig = plt.figure(figsize=(16, 16))
+    ax = fig.add_subplot(111)
+    ax.scatter(TX_r[:, 0], TX_r[:, 1], c=TZ.argmax(axis=1), lw=0, alpha=0.2)
+    plt.savefig('/nthome/maugust/thesis/tsne_data.pdf')
+    plt.show()
+
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
+    print args
+    visualize_tsne(args)
+
+
--- visualization/tsne_model.py
+++ visualization/tsne_model.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Usage: tsne.py <model> <data> <mode>
+"""Usage: tsne_model.py <model> <data> <mode>
 
 """
 
@@ -26,34 +26,30 @@ def visualize_tsne():
                 trainer = cPickle.load(fp)
                 trainer.model.parameters.data[...] = trainer.best_pars
                 data = h5.File(data_dir,'r')
-                TX = data['test_set/test_set']
+                TX = data['test_set/test_set'][...]
                 TZ = data['test_labels/real_test_labels']
                 TZ = one_hot(TZ,13)
-                n_input = TX.shape[1]
-                tsne = Tsne(n_input, 2)
-                TX_r = tsne.fit_transform(TX)
+                print 'data loaded.'
 
                 if args['<mode>'] == 'cnn':
                     f_transformed = trainer.model.function(['inpt'],'mlp-layer-1-ouput')
                 else:
                     f_transformed = trainer.model.function(['inpt'],'layer-1-ouput')
 
+                print 'transform-function generated.'
+
                 trans_TX = f_transformed(TX)
+                print 'data transformed'
                 trans_n_input = trans_TX.shape[1]
                 trans_tsne = Tsne(trans_n_input, 2)
+                print 'TSNE initialized.'
                 trans_TX_r = trans_tsne.fit_transform(trans_TX)
-
-
-                fig = plt.figure(figsize=(16, 16))
-                ax = fig.add_subplot(111)
-                ax.scatter(TX_r[:, 0], TX_r[:, 1], c=TZ.argmax(axis=1), lw=0, alpha=0.2)
-                plt.savefig('./tsne_input.pdf')
-                plt.show()
+                print 'data TSNEd'
 
                 fig = plt.figure(figsize=(16, 16))
                 ax = fig.add_subplot(111)
                 ax.scatter(trans_TX_r[:, 0], trans_TX_r[:, 1], c=TZ.argmax(axis=1), lw=0, alpha=0.2)
-                plt.savefig('./tsne_transformed.pdf')
+                plt.savefig('/nthome/maugust/thesis/tsne_model.pdf')
                 plt.show()
 
 if __name__ == '__main__':
